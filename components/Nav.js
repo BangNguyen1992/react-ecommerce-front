05e5953ff8e1c6837360e84d3b5758374f27f114
BangNguyen1992 <nguyenbahaibang@gmail.com>
2019-04-21 02:43:53 +0300

    Initial commit

---
+++ components/Nav.js
@@ -0,0 +1,35 @@
+// import React from "react";
+import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
+
+const Nav = () => {
+  return (
+    <NavStyles>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+
+      <Link href="/items">
+        <a>Items</a>
+      </Link>
+
+      <Link href="/sell">
+        <a>Sell</a>
+      </Link>
+
+      <Link href="/signup">
+        <a>Signup</a>
+      </Link>
+
+      <Link href="/orders">
+        <a>Orders</a>
+      </Link>
+
+      <Link href="/account">
+        <a>Account</a>
+      </Link>
+    </NavStyles>
+  );
+};
+
+export default Nav;
