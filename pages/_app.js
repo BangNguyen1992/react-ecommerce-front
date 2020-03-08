e532f98ee24d51943925e0be50a11cf812428c57
BangNguyen1992 <nguyenbahaibang@gmail.com>
2020-03-08 03:14:30 +0200

    Update for new changes in Next

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
-import App, { Container } from "next/app";
-import Page from "../components/Page";
+// Custom `App` component to initialize pages
+// https://nextjs.org/docs/advanced-features/custom-app
 
+import App from "next/app";
+import Page from "../components/Page";
 
 export default class MyApp extends App {
   render() {
     const { Component } = this.props;
 
     return (
-      <Container>
-        <Page>
-          <Component />
-        </Page>
-      </Container>
+      <Page>
+        <Component />
+      </Page>
     );
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,19 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+// A custom Document is commonly used to augment your application's <html> and <body> tags.
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static async getInitialProps (ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -21,9 +22,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         )
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
