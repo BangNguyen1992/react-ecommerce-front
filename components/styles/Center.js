34061727a99a662cd0c612b009cce5a657d0b2d0
Bang Nguyen <nguyenbahaibang@gmail.com>
2020-07-26 19:01:14 +0300

    Restructure styled components

---
+++ components/styles/Center.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const CenterText = styled.div`
+  text-align: center;
+`;
+
+export const CenterFlex = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CenterFlexHorizontal = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const CenterFlexVertical = styled.div`
+  display: flex;
+  align-items: center;
+`;
---
+++ components/styles/index.js
@@ -0,0 +1,34 @@
+import ItemStyles from "./ItemStyles";
+import NavStyles from "./NavStyles";
+import OrderItemStyles from "./OrderItemStyles";
+import OrderStyles from "./OrderStyles";
+import PaginationStyles from "./PaginationStyles";
+import PriceTag from "./PriceTag";
+import SickButton from "./SickButton";
+import Supreme from "./Supreme";
+import Table from "./Table";
+import Title from "./Title";
+import Form from "./Form";
+import * as DropDown from "./DropDown";
+import CloseButton from "./CloseButton";
+import CartStyles from "./CartStyles";
+
+import * as Center from "./Center";
+
+export {
+  CartStyles,
+  CloseButton,
+  DropDown,
+  Form,
+  ItemStyles,
+  NavStyles,
+  OrderItemStyles,
+  OrderStyles,
+  PaginationStyles,
+  PriceTag,
+  SickButton,
+  Supreme,
+  Table,
+  Title,
+  Center,
+};
