0db604cec1fd5ad3994558968d624785894e5adb
Bang Nguyen <nguyenbahaibang@gmail.com>
2020-07-26 18:59:45 +0300

    Using react hook for custom app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,62 @@
 // Custom `App` component to initialize pages
 // https://nextjs.org/docs/advanced-features/custom-app
 
-import App from "next/app";
+import { ApolloProvider } from "react-apollo";
 import Page from "../components/Page";
+import withData from "../lib/withData";
 
-export default class MyApp extends App {
-  render() {
-    const { Component } = this.props;
+const MyApp = ({ Component, pageProps, apollo }) => {
 
-    return (
+  return (
+    <ApolloProvider client={apollo}>
       <Page>
-        <Component />
+        <Component {...pageProps} />
       </Page>
-    );
-  }
+    </ApolloProvider>
+  )
 }
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {}
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+  // this exposes the query to the user
+  pageProps.query = ctx.query
+  return { pageProps }
+}
+
+
+export default withData(MyApp)
+
+// import App, { Container } from 'next/app';
+// import Page from '../components/Page';
+// import { ApolloProvider } from 'react-apollo';
+// import withData from '../lib/withData';
+
+// class MyApp extends App {
+//   static async getInitialProps({ Component, ctx }) {
+//     let pageProps = {};
+//     if (Component.getInitialProps) {
+//       pageProps = await Component.getInitialProps(ctx);
+//     }
+//     // this exposes the query to the user
+//     pageProps.query = ctx.query;
+//     return { pageProps };
+//   }
+//   render() {
+//     const { Component, apollo, pageProps } = this.props;
+//     console.log('object apollo', this.props)
+//     return (
+//       <Container>
+//         <ApolloProvider client={apollo}>
+//           <Page>
+//             <Component {...pageProps} />
+//           </Page>
+//         </ApolloProvider>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default withData(MyApp);
