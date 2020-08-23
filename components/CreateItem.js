6d55d45626ec88f4bcf0437935421cfc32c653a8
Bang Nguyen <nguyenbahaibang@gmail.com>
2020-08-23 14:19:47 +0300

    Improve error handler when upload iamge

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 // import PropTypes from "prop-types";
-import { Controller, useForm } from "react-hook-form";
-import { gql, useMutation } from "@apollo/client";
-import Router from "next/router";
-import { Form, ErrorMessage } from "./styles/Form";
-import formatMoney from "../lib/formatMoney";
-import Error from "./ErrorMessage";
-import { transformUrlString } from "../lib/utilities";
+import { Controller, useForm } from 'react-hook-form'
+import { gql, useMutation } from '@apollo/client'
+import Router from 'next/router'
+import styled from 'styled-components'
+// import axios from 'axios'
+import { Form, ErrorMessage } from './styles/Form'
+import formatMoney from '../lib/formatMoney'
+import Error from './ErrorMessage'
+import { transformUrlString } from '../lib/utilities'
 
 export const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -29,69 +31,95 @@ export const CREATE_ITEM_MUTATION = gql`
       price
     }
   }
-`;
+`
 
 // prettier-ignore
 const uploadApi = "https://api.cloudinary.com/v1_1/bangnguyen-1992/image/upload";
+const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png']
+
+const PreviewImage = styled.img`
+  max-width: 100%;
+  max-height: 24rem;
+`
 
 const CreateItem = (props) => {
-  const [loading, setLoading] = useState(false);
-  const { register, control, handleSubmit, errors } = useForm();
-  const [
-    createItem,
-    { loading: mutationLoading, error: mutationError },
-  ] = useMutation(CREATE_ITEM_MUTATION);
+  const [loading, setLoading] = useState(false)
+  const [previewImage, setPreviewImage] = useState(null)
+  const [imageError, setImageError] = useState(null)
+  const { register, control, handleSubmit, errors } = useForm()
+  // prettier-ignore
+  const [createItem, { loading: mutationLoading, error: mutationError }] = useMutation(CREATE_ITEM_MUTATION);
+
+  const handleClick = (e) => {
+    setImageError(null)
+    setPreviewImage(null)
+  }
+
+  const handlePreviewImage = async ({ target }) => {
+    const file = target.files[0]
+    if (file) {
+      if (acceptedImageTypes.includes(file['type'])) {
+        return setPreviewImage(URL.createObjectURL(file))
+      }
+      return setImageError('Only support image file')
+    }
+    // setImageError(null)
+  }
+
+  const uploadImage = async (image) => {
+    if (!imageError) {
+      const imageData = new FormData()
+      imageData.append('file', image)
+      imageData.append('upload_preset', 'e-commerce')
+
+      return await fetch(uploadApi, {
+        method: 'POST',
+        body: imageData,
+      })
+        .then((res) => {
+          if (res.status >= 400 && res.status < 600)
+            return res.json().then((value) => {
+              throw value.error.message
+            })
+
+          return res
+        })
+        .catch((error) => {
+          setImageError(error.message || error)
+          return error
+        })
+    }
+    throw 'Only support image file'
+  }
 
   const onSubmit = async (data, event) => {
-    setLoading(true);
+    setLoading(true)
     try {
-      const imageData = new FormData();
-      imageData.append("file", data.image[0]);
-      imageData.append("upload_preset", "e-commerce");
-
-      const res = await fetch(uploadApi, {
-        method: "POST",
-        body: imageData,
-      });
-
-      const uploadImageData = await res.json();
+      const response = await uploadImage(data.image[0])
+      const imageData = await response.json()
 
       const payload = {
         ...data,
-        image: uploadImageData.eager[0]?.secure_url,
-        largeImage: uploadImageData.secure_url,
-      };
+        image: imageData.eager[0].secure_url,
+        largeImage: imageData.secure_url,
+      }
 
-      const newItem = await createItem({ variables: { ...payload } });
+      const newItem = await createItem({ variables: { ...payload } })
+      const { title, id } = newItem.data.createItem
 
-      const { title, id } = newItem.data.createItem;
-      event.target.reset();
-      setLoading(false);
+      event.target.reset()
+      setLoading(false)
+      setPreviewImage(null)
 
       Router.push({
-        pathname: "/item",
+        pathname: '/item',
         query: { name: transformUrlString(title), id },
-      });
+      })
     } catch (e) {
-      setLoading(false);
-      console.error("Error CREATE_ITEM_MUTATION: ", e);
+      setLoading(false)
+      console.error('Error CREATE_ITEM_MUTATION: ', e)
     }
-  };
-
-  // const handleUpload = async ({ target }) => {
-  //   console.log("object target", target.files);
-  //   const files = target.files;
-  //   const data = new FormData();
-  //   data.append("file", files[0]);
-  //   data.append("upload_preset", "e-commerce");
-
-  //   const res = await fetch(uploadApi, {
-  //     method: "POST",
-  //     body: data,
-  //   });
-  //   const file = await res.json();
-  //   console.log("object file", file);
-  // };
+  }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -104,7 +132,7 @@ const CreateItem = (props) => {
             type="text"
             defaultValue=""
             placeholder="Title"
-            ref={register({ required: "Title is required" })}
+            ref={register({ required: 'Title is required' })}
           />
           {errors.title && <ErrorMessage>{errors.title.message}</ErrorMessage>}
         </label>
@@ -124,7 +152,7 @@ const CreateItem = (props) => {
             )}
             control={control}
             name="price"
-            rules={{ required: "Price is required" }}
+            rules={{ required: 'Price is required' }}
           />
           {errors.price && <ErrorMessage>{errors.price.message}</ErrorMessage>}
         </label>
@@ -136,7 +164,7 @@ const CreateItem = (props) => {
             name="description"
             defaultValue={null}
             placeholder="Description"
-            ref={register({ required: "Description is required" })}
+            ref={register({ required: 'Description is required' })}
           />
           {errors.description && (
             <ErrorMessage>{errors.description.message}</ErrorMessage>
@@ -150,20 +178,22 @@ const CreateItem = (props) => {
             name="image"
             type="file"
             placeholder="Upload an image"
-            ref={register}
-            // onChange={handleUpload}
-            ref={register({ required: "Image is required" })}
+            onChange={handlePreviewImage}
+            onClick={handleClick}
+            ref={register({ required: 'Image is required' })}
           />
+          {previewImage && <PreviewImage src={previewImage} alt="image" />}
           {errors.image && <ErrorMessage>{errors.image.message}</ErrorMessage>}
+          {imageError && <ErrorMessage>{imageError}</ErrorMessage>}
         </label>
 
         <button type="submit">Submit</button>
       </fieldset>
       {mutationError && <Error error={mutationError} />}
     </Form>
-  );
-};
+  )
+}
 
-CreateItem.propTypes = {};
+CreateItem.propTypes = {}
 
-export default CreateItem;
+export default CreateItem
